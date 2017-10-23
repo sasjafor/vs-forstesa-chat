1e4873f5a4ea7951fc1c6c6224cc45211bf6fcf2
Sascha <sascha.forster@hotmail.ch>
2017-10-23 18:39:12 +0200

    added skeleton files

---
+++ app/src/main/java/ch/ethz/inf/vs/a3/clock/VectorClockComparator.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.a3.clock;
+
+import java.util.Comparator;
+
+import ch.ethz.inf.vs.a3.solution.clock.VectorClock;
+
+
+public class VectorClockComparator implements Comparator<VectorClock> {
+
+    @Override
+    public int compare(VectorClock lhs, VectorClock rhs) {
+        // Write your code here
+
+        return 0;
+    }
+}
